28e977650e7a1f5375f602a0b7e07949cfc56622
Jeongjewww <gwmelody99@gmail.com>
2023-03-30 01:28:23 +0900

    variables update

--- terraform/dev/terraform.tfvars
+++ terraform/dev/terraform.tfvars
@@ -1,33 +1,33 @@
 env = "dev"
 
 # iam
-user_alias = "ziwoo"
+user_alias  = "tfuser"
 group_alias = "Terraform"
 
 # vpc
 vpc_cidr_block = "10.0.0.0/16"
-azs = ["ap-northeast-2a", "ap-northeast-2c"]
+azs            = ["ap-northeast-2a", "ap-northeast-2c"]
 
 # key
 div = ["public", "private"]
 
 # ec2
 instance_type = "t2.micro"
-key = ["tf-public-key", "tf-private-key"]
-size = 50
-type = "gp2"
+key           = ["tf-public-key", "tf-private-key"]
+size          = 50
+type          = "gp2"
 # nginx_user_data = "/Users/ziwoo/Documents/GitHub/Iac-study/terraform/modules/ec2/nginx.sh"
 
 # db
-storage_size = 20
-database_name = "tf_database"
-engine = "mariadb"
-engine_version = "10.6"
-class = "db.t3.micro"
-db_username = "admin"
-db_password = "ziwoo99!!"
-bool = [true, false]
-family = "mariadb10.6"
+storage_size      = 20
+database_name     = "tf_database"
+engine            = "mariadb"
+engine_version    = "10.6"
+class             = "db.t3.micro"
+db_username       = "admin"
+db_password       = "ziwoo99!!"
+bool              = [true, false]
+family            = "mariadb10.6"
 subnet_group_name = "rds-subnet"
-param_group_name = "rds-param"
+param_group_name  = "rds-param"
 option_group_name = "rds-option"
--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -1,50 +1,50 @@
 variable "env" {
-  type = string
+  type        = string
   description = "dev"
 }
 
 variable "div" {
-  type = list
+  type        = list(any)
   description = "key division"
 }
 
 variable "user_alias" {
-  type = string
+  type        = string
   description = "iam_user_name"
 }
 
 variable "group_alias" {
-  type = string
+  type        = string
   description = "iam_group_name"
 }
 
 variable "vpc_cidr_block" {
-  type = string
+  type        = string
   description = "vpc_cidr_block"
 }
 
 variable "azs" {
-  type = list
+  type        = list(any)
   description = "availability zone in Seoul"
 }
 
 variable "instance_type" {
-  type = string
+  type        = string
   description = "instance type of ec2"
 }
 
 variable "key" {
-  type = list
+  type        = list(any)
   description = "key for instances"
 }
 
 variable "size" {
-  type = number
+  type        = number
   description = "ebs size"
 }
 
 variable "type" {
-  type = string
+  type        = string
   description = "ebs type"
 }
 
@@ -54,61 +54,61 @@ variable "type" {
 # }
 
 variable "database_name" {
-  type = string
+  type        = string
   description = "database name/identifier"
 }
 
 variable "storage_size" {
-  type = number
+  type        = number
   description = "allocated storage size of db instance"
 }
 
 variable "engine" {
-  type = string
+  type        = string
   description = "name of db engine"
 }
 
 variable "engine_version" {
-  type = string
+  type        = string
   description = "version of db engine"
 }
 
 variable "class" {
-  type = string
+  type        = string
   description = "db instance class"
 }
 
 variable "db_username" {
-  type = string
+  type        = string
   description = "username of database"
 }
 
 variable "db_password" {
-  type = string
+  type        = string
   description = "password of database"
 }
 
 variable "bool" {
-  type = list
-  description = "boolean value of database options"  
+  type        = list(any)
+  description = "boolean value of database options"
 }
 
 variable "family" {
-  type = string
+  type        = string
   description = "family name of db param group"
 }
 
 variable "subnet_group_name" {
-  type = string
+  type        = string
   description = "subnet group name for database"
 }
 
 variable "param_group_name" {
-  type = string
+  type        = string
   description = "param group name for database"
 }
 
 variable "option_group_name" {
-  type = string
+  type        = string
   description = "option group name for database"
 }
