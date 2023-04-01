3ae3cc92756c0252eb4d93e55ad34b98886eebc3
Jeongjewww <gwmelody99@gmail.com>
2023-04-01 18:29:30 +0900

    update: backend module

--- terraform/global/main.tf
+++ terraform/global/main.tf
@@ -26,11 +26,3 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
     type = "S"
   }
 }
-
-# output "s3_bucket" {
-#   value = aws_s3_bucket.tfstate.arn
-# }
-
-# output "dynamodb_lock" {
-#   value = aws_dynamodb_table.terraform_state_lock.arn
-# }
